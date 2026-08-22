525170de09468cf840936fc3c7fca5537fe8c8e7
yhirose <yuji.hirose.bug@gmail.com>
2026-08-21 22:22:07 -0400

    Bump version to 0.3.0

--- include/culebra.h
+++ include/culebra.h
@@ -3,7 +3,7 @@
 // Single source of truth for the version. `culebra --version` prints it, and
 // playground/build.sh greps this line to stamp the Playground title, so keep
 // the `#define CULEBRA_VERSION "X.Y.Z"` form on one line.
-#define CULEBRA_VERSION "0.2.0"
+#define CULEBRA_VERSION "0.3.0"
 
 #include "interpreter.h"
 #include "debugger.h"
